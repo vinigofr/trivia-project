8c7fbf3046d427d875678449f99c4d60621aa7b1
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-10-19 21:40:19 -0300

    Updates

---
+++ precache-manifest.611cb48be5579fcba66217a65bec1f51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4129208c0f018a2509ab34d00591cd55",
+    "url": "/trivia-project/index.html"
+  },
+  {
+    "revision": "565dc3731fb849fc4cca",
+    "url": "/trivia-project/static/css/main.259a4f8f.chunk.css"
+  },
+  {
+    "revision": "3970f20f52ca982920a1",
+    "url": "/trivia-project/static/js/2.156ea8ea.chunk.js"
+  },
+  {
+    "revision": "5d1b8067085602e259fd0862f57df793",
+    "url": "/trivia-project/static/js/2.156ea8ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "565dc3731fb849fc4cca",
+    "url": "/trivia-project/static/js/main.632eb55d.chunk.js"
+  },
+  {
+    "revision": "48409a91e03e6710f1a5",
+    "url": "/trivia-project/static/js/runtime-main.55ba4fb3.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia-project/static/media/trivia.466d153e.png"
+  }
+]);
